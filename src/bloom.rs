26697345bc6f7f21b22c460e9f7099504004b3d0
chrissap543 <cs79@illinois.edu>
2023-11-25 00:06:01 -0600

    added bloom tests

--- src/bloom.rs
+++ src/bloom.rs
@@ -43,3 +43,42 @@ impl<T> BloomFilter<T> {
         is_in
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::BloomFilter;
+
+    fn hash(num: &u32) -> usize {
+        *num as usize
+    }
+    fn hash2(num: &u32) -> usize {
+        ((*num) * 7) as usize
+    }
+    fn hash3(num: &u32) -> usize {
+        ((*num) * 11 % 5144) as usize
+    }
+
+    #[test]
+    fn insert() {
+        let mut filter = BloomFilter::with_size(32, vec![hash, hash2, hash3]); 
+        filter.insert(5); 
+
+        assert_eq!(filter.bits.get(5), Some(true)); 
+        assert_eq!(filter.bits.get(3), Some(true)); 
+        assert_eq!(filter.bits.get(23), Some(true)); 
+    }
+
+    #[test]
+    fn contains() {
+        let mut filter = BloomFilter::with_size(32, vec![hash, hash2, hash3]); 
+        let vals = [5, 25, 33, 44]; 
+        
+        for val in &vals {
+            filter.insert(*val); 
+        }
+
+        for val in &vals {
+            assert!(filter.contains(*val)); 
+        }
+    }
+}
